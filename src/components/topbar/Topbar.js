c8f84dc0066e39120a7b1c4b1260a2d2157dc375
Washington <wyagualmindiola@gmail.com>
2022-03-10 16:10:55 -0500

    first commit

--- src/components/topbar/Topbar.js
+++ src/components/topbar/Topbar.js
@@ -10,7 +10,7 @@ export default function Topbar() {
     <div className="topbar">
       <div className="topbarWrapper">
         <div className="topLeft">
-          <span className="logo">lamaadmin</span>
+          <span className="logo">washington</span>
         </div>
         <div className="topRight">
           <div className="topbarIconContainer">
